d07dd79aba18afbc5dfb79b57d0a3a000278d103
GJ Tiquia <gershom.tiquia@gmail.com>
2024-11-07 20:26:38 -0500

    feat: conditional rendering of the button based on if have an input

--- islands/EnterRoomUI.tsx
+++ islands/EnterRoomUI.tsx
@@ -1,10 +1,16 @@
-import { useRef } from "preact/hooks";
+import { useRef, useState } from "preact/hooks";
 
 export default function EnterRoomUI() {
     const inputRef = useRef<HTMLInputElement>(null);
+    const [showButton, setShowButton] = useState(false);
 
     function enterRoomButtonOnClick() {
         console.log(`enter room clicked! input: ${inputRef.current?.value}`);
+        // TODO : Redirect to /room/[roomName]
+    }
+
+    function onInputUpdate(inputString: string) {
+        setShowButton(inputString.length > 0);
     }
 
     return (
@@ -12,14 +18,41 @@ export default function EnterRoomUI() {
             <h2 class={"font-bold"}>ENTER A ROOM NAME</h2>
             <input
                 ref={inputRef}
+                onInput={(e) => onInputUpdate(e.currentTarget.value)}
                 class={"w-full max-w-sm border-2 border-gray-500 rounded-md text-center p-1"}
             >
             </input>
-            {/* //TODO: conditional render the hint or the button based on if hv text in input component */}
-            <p>
-                <em>(dont worry, any name will do)</em>
-            </p>
-            <button onClick={enterRoomButtonOnClick}>Enter Room</button>
+            {showButton
+                ? (
+                    <EnterRoomButton
+                        enterRoomButtonOnClick={enterRoomButtonOnClick}
+                    />
+                )
+                : <EnterRoomNameHint />}
         </>
+    );
+}
+
+function EnterRoomButton(
+    props: { enterRoomButtonOnClick: () => void },
+) {
+    // TODO : i wonder if its possible to be an anchor, and the href is from the props
+    return (
+        <div class={"pt-2"}>
+            <button
+                class={"bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-bold py-1 px-8 rounded-md"}
+                onClick={props.enterRoomButtonOnClick}
+            >
+                Enter Room
+            </button>
+        </div>
+    );
+}
+
+function EnterRoomNameHint() {
+    return (
+        <p>
+            <em>(dont worry, any name will do)</em>
+        </p>
     );
 }
