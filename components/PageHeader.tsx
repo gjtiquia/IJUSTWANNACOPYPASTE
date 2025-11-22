8722c9dcc8d58c401392713ead779b190e65fb39
GJ Tiquia <gershom.tiquia@gmail.com>
2025-11-22 18:47:12 -0500

    remove everything

--- components/PageHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageHeader() {
-    return (
-        <h1 class={"font-bold text-2xl sm:text-3xl"}>
-            <a href={"/"}>I JUST WANNA COPY PASTE</a>
-        </h1>
-    );
-}
--- routes/r/[roomName].tsx
+++
@@ -1,8 +0,0 @@
-import { PageProps } from "$fresh/server.ts";
-import RoomIsland from "../../islands/RoomIsland.tsx";
-
-export default function RoomPage(
-    props: PageProps & { params: { roomName: string } },
-) {
-    return <RoomIsland roomName={props.params.roomName} />;
-}
