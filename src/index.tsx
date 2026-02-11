67cd6a47533f187f497024ccd89e8725651c59d3
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-10 20:02:56 -0500

    help keywords

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { HomePage } from "./pages/HomePage";
 
 const store = new Map<string, string>();
 
+const helpKeywords = ["help", "h", "man", "manual", "usage"];
+function isHelpKeyword(value: string) {
+    return helpKeywords.includes(value.toLowerCase().trim());
+}
+
 const app = new Elysia()
     .use(html())
     .onError(({ request, error }) => {
@@ -17,10 +22,13 @@ const app = new Elysia()
         }
     })
 
-    // TODO : should have a list of reserved keywords
-    .get("help", ({ redirect }) => redirect("/"))
+    .get("/help", ({ redirect }) => redirect("/"))
 
-    .get(":room", ({ params, request, status, redirect }) => {
+    .get("/:room", ({ params, request, status, redirect }) => {
+        if (isHelpKeyword(params.room)) {
+            return redirect("/help");
+        }
+
         const roomExists = store.has(params.room);
         if (roomExists) {
             const value = store.get(params.room)!;
@@ -42,21 +50,33 @@ const app = new Elysia()
             }
         }
     })
-    .post(":room", async ({ params, request, status }) => {
+
+    .post("/:room", async ({ params, request, status }) => {
+        if (isHelpKeyword(params.room)) {
+            return status(
+                400,
+                newline(
+                    `${params.room} is a reserved keyword and cannot be used!`,
+                ),
+            );
+        }
+
         const text = await request.text();
         store.set(params.room, text);
         return newline(`room ${params.room}: ${store.get(params.room)}`);
     })
-    .all("*", ({ request, status, redirect }) => {
+
+    .all("/*", ({ request, status, redirect }) => {
         if (isCurl(request)) {
             return status(
                 400,
-                newline("Error! curl -L /help for usage instructions."),
+                newline("Error! curl /help for usage instructions."),
             );
         } else {
             redirect("/");
         }
     })
+
     .listen(process.env.PORT ?? 3000);
 
 console.log(
